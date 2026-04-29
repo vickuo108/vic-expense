5d6853a7ae601f5be719966538869538b9875a3e
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-04-30 02:48:21 +0800

    fix: add skipWaiting and clients.claim for instant SW update

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE = 'vic-v2-2';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html']))));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+const CACHE = 'vic-v2-3';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
+});
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => clients.claim())
+));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
