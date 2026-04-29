78f17219c42d7d102be58717ed26d35806fcfe2f
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-04-30 01:43:00 +0800

    Initial commit: Vic 記帳本 v2 PWA

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'vic-v2-1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html']))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
